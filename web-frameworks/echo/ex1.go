ac0f417630c2999669dd6d505d68420d9d00fb20
jafar75 <gholamzadeh.cuh@gmail.com>
2024-02-07 12:14:03 +0330

    simple example for web framework - echo

--- web-frameworks/echo/ex1.go
+++ web-frameworks/echo/ex1.go
@@ -6,10 +6,26 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// User
+type User struct {
+	Name  string `json:"name" xml:"name"`
+	Email string `json:"email" xml:"email"`
+}
+
+// Handler
+func getUsers(c echo.Context) error {
+	u := &User{
+	  Name:  "Jeff",
+	  Email: "gholamzadeh.cuh@gmail.com",
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/users", getUsers)
 	e.Logger.Fatal(e.Start(":1323"))
 }
