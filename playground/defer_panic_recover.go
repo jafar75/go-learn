1a0fbc2abc7e06faf971b80dd9e3d5263aa25500
jafar75 <gholamzadeh.cuh@gmail.com>
2024-03-01 20:29:39 +0330

    short comment

--- playground/defer_panic_recover.go
+++ playground/defer_panic_recover.go
@@ -1,4 +1,5 @@
 // recover() method captures what the panic() is called with
+// calls to defer() is pushed to a list and will be executed in a LIFO manner
 
 package main
 
@@ -7,27 +8,27 @@ import "fmt"
 const number = 3
 
 func main() {
-    f()
-    fmt.Println("Returned normally from f.")
+	f()
+	fmt.Println("Returned normally from f.")
 }
 
 func f() {
-    defer func() {
-        if r := recover(); r != nil {
-            fmt.Println("Recovered in f, panic arg: ", r)
-        }
-    }()
-    fmt.Println("Calling divide.")
-    divide(number, 0)
-    fmt.Println("Returned normally from divide.")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f, panic arg: ", r)
+		}
+	}()
+	fmt.Println("Calling divide.")
+	divide(number, 0)
+	fmt.Println("Returned normally from divide.")
 }
 
 func divide(number int, i int) {
-    if i > number {
-        fmt.Println("Panicking!")
-        panic(fmt.Sprintf("panic called with %v", i))
-    }
-    defer fmt.Println("Defer in divide", i)
-    fmt.Println("Printing in divide", i)
-    divide(number, i + 1)
+	if i > number {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("panic called with %v", i))
+	}
+	defer fmt.Println("Defer in divide", i)
+	fmt.Println("Printing in divide", i)
+	divide(number, i+1)
 }
