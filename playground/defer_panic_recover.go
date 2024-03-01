2983908ce9a63b6f9c36a041971bfdbebe982863
jafar75 <gholamzadeh.cuh@gmail.com>
2024-03-01 20:29:18 +0330

    simple example of defer, panic, recover

---
+++ playground/defer_panic_recover.go
@@ -0,0 +1,33 @@
+// recover() method captures what the panic() is called with
+
+package main
+
+import "fmt"
+
+const number = 3
+
+func main() {
+    f()
+    fmt.Println("Returned normally from f.")
+}
+
+func f() {
+    defer func() {
+        if r := recover(); r != nil {
+            fmt.Println("Recovered in f, panic arg: ", r)
+        }
+    }()
+    fmt.Println("Calling divide.")
+    divide(number, 0)
+    fmt.Println("Returned normally from divide.")
+}
+
+func divide(number int, i int) {
+    if i > number {
+        fmt.Println("Panicking!")
+        panic(fmt.Sprintf("panic called with %v", i))
+    }
+    defer fmt.Println("Defer in divide", i)
+    fmt.Println("Printing in divide", i)
+    divide(number, i + 1)
+}
