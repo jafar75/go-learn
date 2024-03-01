111266020f764779ea8f0ab56b16f4cdd6eda328
jafar75 <gholamzadeh.cuh@gmail.com>
2024-03-01 05:25:38 +0330

    apply missing go fmt

--- DSA/Recursion/pow.go
+++ DSA/Recursion/pow.go
@@ -8,15 +8,15 @@ func main() {
 	var x float64 = 2.10000
 	var n int = 3
 
-    var neg bool = false
-    if n < 0 {
-        neg = true
-    }
-    out := PowPositive(x, uint(Abs(n)))
-    if neg {
-        out = 1 / out
-    }
-    fmt.Println("pow(x, n) = ", out)
+	var neg bool = false
+	if n < 0 {
+		neg = true
+	}
+	out := PowPositive(x, uint(Abs(n)))
+	if neg {
+		out = 1 / out
+	}
+	fmt.Println("pow(x, n) = ", out)
 }
 
 func Abs(n int) uint {
@@ -27,16 +27,16 @@ func Abs(n int) uint {
 }
 
 func PowPositive(x float64, n uint) float64 {
-    if n == 0 {
-        return 1.0
-    } else if n == 1 {
-        return x
-    }
+	if n == 0 {
+		return 1.0
+	} else if n == 1 {
+		return x
+	}
 
-    var out float64 = x
-    p := uint(1)
-    for ; p < n; p = p * 2 {
-        out = out * out
-    }
-    return out / PowPositive(x, p - n)
+	var out float64 = x
+	p := uint(1)
+	for ; p < n; p = p * 2 {
+		out = out * out
+	}
+	return out / PowPositive(x, p-n)
 }
